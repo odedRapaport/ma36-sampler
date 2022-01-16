fc32593230c8c9f0fd69924f9792603ec221fa2e
odedRapaport <rapaportoded@gmail.com>
2022-01-16 18:13:11 +0200

    add MadaReport class

---
+++ ThePerpetualSample/src/main/java/thrPerpetualSample/data/MadaReport.java
@@ -0,0 +1,116 @@
+package thrPerpetualSample.data;
+
+public class MadaReport {
+    private String mdaCode, idNum, idType, firstName, lastName, city, street, buildingNumber, barcode, getDate, takeDate, resultDate;
+
+    public MadaReport(String mdaCode, String idNum, String idType, String firstName, String lastName, String city, String street, String buildingNumber, String barcode, String getDate, String takeDate, String resultDate) {
+        this.mdaCode = mdaCode;
+        this.idNum = idNum;
+        this.idType = idType;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.street = street;
+        this.buildingNumber = buildingNumber;
+        this.barcode = barcode;
+        this.getDate = getDate;
+        this.takeDate = takeDate;
+        this.resultDate = resultDate;
+    }
+
+    public String getMdaCode() {
+        return mdaCode;
+    }
+
+    public void setMdaCode(String mdaCode) {
+        this.mdaCode = mdaCode;
+    }
+
+    public String getIdNum() {
+        return idNum;
+    }
+
+    public void setIdNum(String idNum) {
+        this.idNum = idNum;
+    }
+
+    public String getIdType() {
+        return idType;
+    }
+
+    public void setIdType(String idType) {
+        this.idType = idType;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getBuildingNumber() {
+        return buildingNumber;
+    }
+
+    public void setBuildingNumber(String buildingNumber) {
+        this.buildingNumber = buildingNumber;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public String getGetDate() {
+        return getDate;
+    }
+
+    public void setGetDate(String getDate) {
+        this.getDate = getDate;
+    }
+
+    public String getTakeDate() {
+        return takeDate;
+    }
+
+    public void setTakeDate(String takeDate) {
+        this.takeDate = takeDate;
+    }
+
+    public String getResultDate() {
+        return resultDate;
+    }
+
+    public void setResultDate(String resultDate) {
+        this.resultDate = resultDate;
+    }
+}
